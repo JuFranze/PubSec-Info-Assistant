654ffe3b84fd8ea3f48d327660cd004bc963f73c
Jufranze <55830611+JuFranze@users.noreply.github.com>
2024-02-20 12:04:48 +0000

    Update background colors and logo

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -7,6 +7,7 @@ import openai from "../../assets/openai.svg";
 import { WarningBanner } from "../../components/WarningBanner/WarningBanner";
 import styles from "./Layout.module.css";
 import { Title } from "../../components/Title/Title";
+import logo from "../../assets/Logo-caha.jpg"
 
 const Layout = () => {
     return (
@@ -15,7 +16,7 @@ const Layout = () => {
                 <WarningBanner />
                 <div className={styles.headerContainer}>
                     <div className={styles.headerTitleContainer}>
-                        <img src={openai} alt="Azure OpenAI" className={styles.headerLogo} />
+                        <img src={logo} alt="Logo" className={styles.headerLogo} />
                         <h3 className={styles.headerTitle}><Title/></h3>
                     </div>
                     <nav>
